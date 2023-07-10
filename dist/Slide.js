6e1253d84bad0541dbe308576ca83c6353f096fb
Alana-Rocha <alanarodrigues.rocha1@gmail.com>
2023-07-10 11:55:38 -0300

    video element

--- dist/Slide.js
+++ dist/Slide.js
@@ -7,8 +7,10 @@ export default class Slide {
     index;
     slide;
     timeout;
-    paused;
     pausedTimeout;
+    paused;
+    thumbItems;
+    thumb;
     constructor(container, slides, controls, time = 5000) {
         this.container = container;
         this.slides = slides;
@@ -16,75 +18,112 @@ export default class Slide {
         this.time = time;
         this.timeout = null;
         this.pausedTimeout = null;
-        this.index = 0;
+        this.index = localStorage.getItem("activeSlide")
+            ? Number(localStorage.getItem("activeSlide"))
+            : 0;
         this.slide = this.slides[this.index];
         this.paused = false;
+        this.thumbItems = null;
+        this.thumb = null;
         this.init();
     }
-    ;
     hide(el) {
-        el.classList.remove('active');
+        el.classList.remove("active");
+        if (el instanceof HTMLVideoElement) {
+            el.currentTime = 0;
+            el.pause();
+        }
     }
-    ;
     show(index) {
         this.index = index;
         this.slide = this.slides[this.index];
+        localStorage.setItem("activeSlide", String(this.index));
+        if (this.thumbItems) {
+            this.thumb = this.thumbItems[this.index];
+            this.thumbItems.forEach((el) => el.classList.remove("active"));
+            this.thumb.classList.add("active");
+        }
         this.slides.forEach((el) => this.hide(el));
-        this.slides[index].classList.add('active');
-        this.auto(this.time);
+        this.slide.classList.add("active");
+        if (this.slide instanceof HTMLVideoElement) {
+            this.autoVideo(this.slide);
+        }
+        else {
+            this.auto(this.time);
+        }
     }
-    ;
+    autoVideo(video) {
+        video.muted = true;
+        video.play();
+        let firstPlay = true;
+        video.addEventListener("playing", () => {
+            if (firstPlay)
+                this.auto(video.duration * 1000);
+            firstPlay = false;
+        });
+    }
     auto(time) {
         this.timeout?.clear();
         this.timeout = new Timeout(() => this.next(), time);
+        if (this.thumb)
+            this.thumb.style.animationDuration = `${time}ms`;
     }
-    ;
     prev() {
         if (this.paused)
             return;
         const prev = this.index > 0 ? this.index - 1 : this.slides.length - 1;
         this.show(prev);
     }
-    ;
     next() {
         if (this.paused)
             return;
-        const next = (this.index + 1) < this.slides.length ? this.index + 1 : 0;
+        const next = this.index + 1 < this.slides.length ? this.index + 1 : 0;
         this.show(next);
     }
-    ;
     pause() {
         this.pausedTimeout = new Timeout(() => {
             this.timeout?.pause();
             this.paused = true;
+            this.thumb?.classList.add("paused");
+            if (this.slide instanceof HTMLVideoElement)
+                this.slide.pause();
         }, 300);
     }
-    ;
     continue() {
         this.pausedTimeout?.clear();
         if (this.paused) {
             this.paused = false;
             this.timeout?.continue();
+            this.thumb?.classList.remove("paused");
+            if (this.slide instanceof HTMLVideoElement)
+                this.slide.play();
         }
     }
-    ;
-    addControl() {
-        const prevButton = document.createElement('button');
-        const nextButton = document.createElement('button');
-        prevButton.innerText = 'Slide Anterior';
-        nextButton.innerText = 'Slide Posterior';
+    addControls() {
+        const prevButton = document.createElement("button");
+        const nextButton = document.createElement("button");
+        prevButton.innerText = "Slide Anterior";
+        nextButton.innerText = "Próximo Slide";
         this.controls.appendChild(prevButton);
         this.controls.appendChild(nextButton);
-        this.controls.addEventListener('pointerdown', () => this.pause());
-        this.controls.addEventListener('pointerup', () => this.continue());
-        nextButton.addEventListener('pointerup', () => this.next());
-        prevButton.addEventListener('pointerup', () => this.prev());
+        this.controls.addEventListener("pointerdown", () => this.pause());
+        this.controls.addEventListener("pointerup", () => this.continue());
+        prevButton.addEventListener("pointerup", () => this.prev());
+        nextButton.addEventListener("pointerup", () => this.next());
     }
-    ;
+    addThumbItems() {
+        const thumbContainer = document.createElement("div");
+        thumbContainer.id = "slide-thumb";
+        for (let i = 0; i < this.slides.length; i++) {
+            thumbContainer.innerHTML += `<span><span class="thumb-item"></span></span>`;
+        }
+        this.controls.appendChild(thumbContainer);
+        this.thumbItems = Array.from(document.querySelectorAll(".thumb-item"));
+    }
     init() {
-        this.addControl();
+        this.addControls();
+        this.addThumbItems();
         this.show(this.index);
     }
-    ;
 }
 //# sourceMappingURL=Slide.js.map
